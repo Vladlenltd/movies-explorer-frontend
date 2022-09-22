8cd56ce06f3486fd338fe25b7330afb8221f8d64
Vladislav Evdokimov <gangrena14@yandex.ru>
2022-09-22 16:24:44 +0300

    feat: начал делать header

---
+++ movies-explorer-frontend/src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import logo from '../../images/logo.svg'
+
+function Header() {
+    return (
+        <img alt="Логотип" src={logo} />
+        )
+}
+
+export default Header;
