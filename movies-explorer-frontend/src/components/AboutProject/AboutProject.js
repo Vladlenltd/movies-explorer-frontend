4a6847860c941ff0dca0fc037efa50fd77197812
Vladislav Evdokimov <gangrena14@yandex.ru>
2022-09-26 18:05:30 +0300

    feat: завершить секцию tech

--- movies-explorer-frontend/src/components/AboutProject/AboutProject.js
+++ movies-explorer-frontend/src/components/AboutProject/AboutProject.js
@@ -3,15 +3,15 @@ import React from 'react';
 function AboutProject() {
   return (
     <section className="about-project">
-      <h2 className="about-project__title">О проекте</h2>
+      <h2 className="about-project__title title">О проекте</h2>
       <ul className="about-project__list description__list">
         <li className="description__list-item">
           <h3 className="description__title">
             Дипломный проект включал 5 этапов
           </h3>
           <p className="description__text">
-            Составление плана, работу над бэкендом, вёрстку, добавление
-            функциональности и финальные доработки.
+            Составление плана, работу над бэкендом, вёрстку,
+            добавление функциональности и финальные доработки.
           </p>
         </li>
         <li className="description__list-item">
@@ -19,8 +19,8 @@ function AboutProject() {
             На выполнение диплома ушло 5 недель
           </h3>
           <p className="description__text">
-            У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было
-            соблюдать, чтобы успешно защититься.
+            У каждого этапа был мягкий и жёсткий дедлайн, которые
+            нужно было соблюдать, чтобы успешно защититься.
           </p>
         </li>
       </ul>
--- movies-explorer-frontend/src/components/Main/Main.js
+++ movies-explorer-frontend/src/components/Main/Main.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import Promo from '../Promo/Promo';
-import AboutProject from '../AboutProject/AboutProject';
+import React from "react";
+import Promo from "../Promo/Promo";
+import AboutProject from "../AboutProject/AboutProject";
+import Techs from "../Techs/Techs";
 
 function Main() {
-  return (
-    <main className="main">
-      <Promo />
-      <AboutProject />
-    </main>
-  );
+    return (
+        <main className="main">
+            <Promo />
+            <AboutProject />
+            <Techs />
+        </main>
+    );
 }
 export default Main;
