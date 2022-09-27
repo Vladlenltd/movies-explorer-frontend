3e0efb54dee4f4c6a92ac480df37419f9ed62f75
Vladislav Evdokimov <gangrena14@yandex.ru>
2022-09-27 17:23:46 +0300

    feat: сделать форму поиска

---
+++ movies-explorer-frontend/src/components/Movies/Movies.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import SearchForm from '../SearchForm/SearchForm';
+
+function Movies() {
+  return <SearchForm />;
+}
+export default Movies;
---
+++ movies-explorer-frontend/src/components/SearchForm/SearchForm.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import FilterCheckbox from '../FilterCheckbox/FilterCheckbox';
+import iconBtn from '../../images/find-btn.svg';
+
+function SearchForm() {
+  return (
+    <form className="search-form" name="movie-search-form">
+      <div className="search-form__wrapper">
+        <input
+          className="search-form__input"
+          placeholder="Фильм"
+          required
+        />
+        <button className="search-form__btn" type="submit">
+          <img
+            className="search-form__icon"
+            src={iconBtn}
+            alt="Поиск"
+          />
+        </button>
+      <FilterCheckbox />
+      </div>
+    </form>
+  );
+}
+export default SearchForm;
