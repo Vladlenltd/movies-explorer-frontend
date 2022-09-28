e100a3c81a8e35e31517a8f41e972e573e4b903e
Vladislav Evdokimov <gangrena14@yandex.ru>
2022-09-28 23:13:06 +0300

    feat: завершить profile

--- movies-explorer-frontend/src/components/App/App.js
+++ movies-explorer-frontend/src/components/App/App.js
@@ -4,6 +4,7 @@ import Header from '../Header/Header';
 import Main from '../Main/Main';
 import Movies from '../Movies/Movies';
 import SavedMovies from '../SavedMovies/SavedMovies';
+import Profile from '../Profile/Profile';
 import './App.css';
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <Footer />
       <Movies />
       <SavedMovies />
+      <Profile />
     </div>
   );
 }
