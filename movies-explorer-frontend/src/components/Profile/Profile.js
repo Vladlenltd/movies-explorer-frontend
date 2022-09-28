8ba673440554330ad92607869c95b4dd34362638
Vladislav Evdokimov <gangrena14@yandex.ru>
2022-09-28 23:13:22 +0300

    feat: завершить profile

--- movies-explorer-frontend/src/components/Profile/Profile.js
+++ movies-explorer-frontend/src/components/Profile/Profile.js
@@ -6,28 +6,28 @@ function Profile({ logOut }) {
   const userName = ' Keks';
   return (
     <section className="profile">
-        <h2 className="profile__title">
-          Привет
-          {userName}
-          !
-        </h2>
-        <form className="profile__form" name="change-profile-form">
-          <div className="profile__container">
-            <input className="profile__input profile__input_name" id="name" />
-            <label className="profile__label profile__label_name" htmlFor="name">Имя</label>
-          </div>
-          <div className="profile__container">
-            <input className="profile__input profile__input_email" id="email" />
-            <label className="profile__label profile__label_email" htmlFor="email">E-mail</label>
-          </div>
-        </form>
+      <h2 className="profile__title">
+        Привет
+        {userName}
+        !
+      </h2>
+      <form className="profile__form" name="change-profile-form">
+        <div className="profile__container">
+          <input className="profile__input profile__input_name" id="name" />
+          <label className="profile__label profile__label_name" htmlFor="name">Имя</label>
+        </div>
+        <div className="profile__container">
+          <input className="profile__input profile__input_email" id="email" />
+          <label className="profile__label profile__label_email" htmlFor="email">E-mail</label>
+        </div>
+      </form>
       <div className="profile__wrapper">
-          <button
-            type="submit"
-            className="profile__btn profile__submit-btn"
-          >
-            Редактировать
-          </button>
+        <button
+          type="submit"
+          className="profile__btn profile__submit-btn"
+        >
+          Редактировать
+        </button>
         <button
           type="button"
           className="profile__btn profile__logout-btn"
