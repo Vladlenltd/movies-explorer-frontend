735da50f08b8c8e7d48e9d6c16e17169f9bb549d
Vladislav Evdokimov <gangrena14@yandex.ru>
2022-12-03 15:39:26 +0300

    feat: написать запрос к серверу регистрации и авторизации

---
+++ movies-explorer-frontend/src/utils/MainApi.js
@@ -0,0 +1,51 @@
+class MainApi {
+    constructor(data) {
+        this._baseUrl = data.baseUrl;
+        this._headers = data.headers;
+    }
+    //проверка статуса сервера
+    _checkStatus(res) {
+        return res.ok ? res.json() : Promise.reject(`Ошибка: $(res.status)`);
+    };
+    //регистрация
+    registration(name, email, password) {
+        return fetch(`${this._baseUrl}/signup`), {
+            method: 'POST',
+            headers: this._headers,
+            body: JSON.stringify({
+                name,
+                email,
+                password,
+            }).then(this._checkStatus)
+        }
+    }
+    //авторизация
+    login(email, password) {
+        return fetch(`${this._baseUrl}/signin`), {
+            method: 'POST',
+            headers: this._headers,
+            body: JSON.stringify({
+                email,
+                password,
+            }).then(this._checkStatus)
+        }
+    }
+    //проверка токена
+    checkTokenValidity(token) {
+        return fetch(`${this._baseUrl}/users/me`, {
+            method: 'GET',
+            headers: {
+                ...this._headers,
+                authorization: `Bearer ${token}`
+            },
+        }).then(this._checkStatus);
+    }
+
+}
+
+export const mainApi = new MainApi({
+    baseUrl: 'http://localhost:3000',
+    headers: {
+        'Content-type': 'application/json'
+    }
+})
