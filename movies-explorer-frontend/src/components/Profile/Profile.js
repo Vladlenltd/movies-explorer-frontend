8ff3299ca44795cf19d0c1a63301e0d1ce1db0ce
Vladislav Evdokimov <gangrena14@yandex.ru>
2023-01-13 17:19:45 +0300

    feat: реализовать функционал изменения профиля

--- movies-explorer-frontend/src/components/Profile/Profile.js
+++ movies-explorer-frontend/src/components/Profile/Profile.js
@@ -1,85 +1,106 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import Header from '../Header/Header';
 import { CurrentUserContext } from '../Context/CurrentUserContext';
 import { ValidationForms } from '../ValidationForms/ValidationForms'
 
 // eslint-disable-next-line react/prop-types
 function Profile({ handleUpdateUser, logOut }) {
-	const currentUser = useContext(CurrentUserContext);
-	const controlInput = ValidationForms();
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [isButtonActive, setIsButtonActive] = useState(false);
+    const currentUser = useContext(CurrentUserContext);
+    const { handleChange, errors, isValid } = ValidationForms();
 
-	const handleSubmit = (e) => {
-		e.preventDefault();
-		const { name, email } = controlInput.values;
-		if (!name) {
-			handleUpdateUser(currentUser.name, email);
-		} else if (!email) {
-			handleUpdateUser(name, currentUser.email);
-		} else {
-			handleUpdateUser(name, email);
-		}
-		controlInput.resetForm();
-	};
+    useEffect(() => {
+        if (name === currentUser.name && email === currentUser.email) {
+            setIsButtonActive(true);
+        } else if (isValid) {
+            setIsButtonActive(false);
+        } else if (!isValid) {
+            setIsButtonActive(true);
+        }
+    }, [email, name, currentUser.name, currentUser.email, isValid])
 
+    useEffect(() => {
+        setName(currentUser.name);
+        setEmail(currentUser.email);
+    }, [currentUser]);
 
-	return (
-		<div className="profile__container">
-			<Header />
-			<main className="profile">
-				<h2 className="profile__title">
-					Привет {currentUser.name}!
-				</h2>
-				<form className="profile__form" name="change-profile-form" noValidate>
-					<div className="profile__container">
-						<input className="profile__input profile__input_name"
-							id="name"
-							name='name'
-							type='text'
-							minLength='5'
-							maxLength='40'
-							required='{true}'
-							placeholder={currentUser}
-							pattern='[A-Za-zА-Яа-яЁё\s-]+'
-							onChange={controlInput.handleChange}
-							value={controlInput?.values?.name ?? currentUser.name}
-						/>
-						<label className="profile__label profile__label_name" htmlFor="name">Имя</label>
-					</div>
-					<div className="profile__container">
-						<input className="profile__input profile__input_email"
-							id="email"
-							name='email'
-							type='email'
-							required='{true}'
-							minLength='5'
-							maxLength='40'
-							placeholder={currentUser.email}
-							pattern='^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$'
-							onChange={controlInput.handleChange}
-							value={controlInput?.values?.email ?? currentUser.email}
-						/>
-						<label className="profile__label profile__label_email" htmlFor="email">E-mail</label>
-					</div>
-				</form>
-				<div className="profile__wrapper">
-					<button
-						type="submit"
-						className="profile__btn profile__submit-btn"
-						onClick={handleSubmit}
-					>
-						Редактировать
-					</button>
-					<button
-						type="button"
-						className="profile__btn profile__logout-btn"
-						onClick={logOut}
-					>
-						Выйти из аккаунта
-					</button>
-				</div>
-			</main>
-		</div>
-	);
+    function handleChangeName(e) {
+        setName(e.target.value);
+        handleChange(e);
+    }
+
+    function handleChangeEmail(e) {
+        setEmail(e.target.value);
+        handleChange(e);
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        handleUpdateUser(name, email);
+    }
+    return (
+        <div className="profile__container">
+            <Header />
+            <main className="profile">
+                <h2 className="profile__title">
+                    Привет {currentUser.name}!
+                </h2>
+                <form className="profile__form" name="change-profile-form" noValidate>
+                    <div className="profile__container">
+                        <input className="profile__input profile__input_name"
+                            id="name"
+                            name='name'
+                            type='text'
+                            minLength='5'
+                            maxLength='40'
+                            required='{true}'
+                            placeholder={currentUser.name}
+                            pattern='[A-Za-zА-Яа-яЁё\s-]+'
+                            onChange={handleChangeName}
+                            value={name || ''}
+                        />
+                        <label className="profile__label profile__label_name" htmlFor="name">Имя</label>
+                        <span className="sign-form__error"><p className="sign-form__error-text">{errors.name || ''}</p></span>
+                    </div>
+                    <div className="profile__container">
+                        <input className="profile__input profile__input_email"
+                            id="email"
+                            name='email'
+                            type='email'
+                            required='{true}'
+                            minLength='5'
+                            maxLength='40'
+                            placeholder={currentUser.email}
+                            pattern='^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$'
+                            onChange={handleChangeEmail}
+                            value={email || ''}
+                        />
+                        <label className="profile__label profile__label_email" htmlFor="email">E-mail</label>
+                        <span className="sign-form__error"><p className="sign-form__error-text">{errors.email || ''}</p></span>
+                    </div>
+                </form>
+                <div className="profile__wrapper">
+                    <button
+                        type="submit"
+                        className="profile__btn profile__submit-btn"
+                        onClick={handleSubmit}
+                        disabled={isButtonActive}
+                    >
+                        Редактировать
+                    </button>
+                    <button
+                        type="button"
+                        className="profile__btn profile__logout-btn"
+                        onClick={logOut}
+                    >
+                        Выйти из аккаунта
+                    </button>
+                </div>
+            </main>
+        </div>
+    );
 }
 export default Profile;
